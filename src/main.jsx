5884e4113bc00c094b66d2e58d21abff590f8bcb
hoyoungleee <ghdud939@gmail.com>
2025-04-24 09:49:04 +0900

    기본틀 적용

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //라우터를 사용하기 위해선 사용하려는 컴포넌트를 
+  //BrowserRouter로 감싸주세요.
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
